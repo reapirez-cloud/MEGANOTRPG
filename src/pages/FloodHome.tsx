e47f7d14e0f0fbc6b864264f0e5ae594690122ae
reapirez-cloud <reapirez@gmail.com>
2026-08-24 12:49:09 +0300

    Create flood showcase screen

---
+++ src/pages/FloodHome.tsx
@@ -0,0 +1,3 @@
+export default function FloodHome() {
+  return <section className="showcase"><h2>💬 Флуд</h2><div className="cards"><article>Общий чат</article><article>Мемы и шутки</article><article>Организация игры</article></div></section>
+}
