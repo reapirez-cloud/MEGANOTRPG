db45d772acc7dede96991098e91cc709e9343222
reapirez-cloud <reapirez@gmail.com>
2026-08-24 12:49:14 +0300

    Create world showcase screen

---
+++ src/pages/WorldHome.tsx
@@ -0,0 +1,3 @@
+export default function WorldHome() {
+  return <section className="showcase"><h2>🌍 Мир</h2><div className="cards"><article>Лор</article><article>Локации</article><article>NPC</article><article>Достижения</article></div></section>
+}
