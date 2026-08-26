c30d770723edad415672bf395c1e565b3d426794
reapirez-cloud <reapirez@gmail.com>
2026-08-26 14:58:36 +0300

    feat(character-engine): expose engine api

---
+++ src/character-engine/index.ts
@@ -0,0 +1,22 @@
+export { abilityModifier, proficiencyBonusForLevel, resolveCharacter } from "./resolver"
+export {
+  ABILITY_KEYS,
+  SKILL_KEYS,
+  type AbilityKey,
+  type BaseCharacter,
+  type CharacterCondition,
+  type CharacterContribution,
+  type CharacterSource,
+  type CharacterState,
+  type GrantContribution,
+  type GrantTarget,
+  type NumericContribution,
+  type NumericOperation,
+  type NumericTarget,
+  type ProficiencyRank,
+  type ResolvedAbility,
+  type ResolvedCharacter,
+  type ResolvedGrant,
+  type ResolvedNumber,
+  type SkillKey,
+} from "./types"
