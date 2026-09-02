137878611d34cfba392d30ba7dd1cad2dee90df5
reapirez-cloud <reapirez@gmail.com>
2026-09-02 08:36:03 +0300

    Repair Druid Voss feature alias

--- src/data/classes/druidVossNarration.ts
+++ src/data/classes/druidVossNarration.ts
@@ -1,9 +1,24 @@
+import {
+  druidClassVossNarration,
+  getDruidBaseVossNarration,
+  getDruidSubclassFeatureVossNarration as getGeminiDruidSubclassFeatureVossNarration,
+  getDruidSubclassVossNarration,
+} from "./druidVossNarrationGemini.ts"
+
 export {
   druidClassVossNarration,
   getDruidBaseVossNarration,
   getDruidSubclassVossNarration,
-  getDruidSubclassFeatureVossNarration,
-} from "./druidVossNarrationGemini.ts"
+}
+
+const featureNameAliases: Record<string, string> = {
+  "spores:Ореол спор и Симбиотическая сущность": "Ореоло спор и Симбиотическая сущность",
+}
+
+export function getDruidSubclassFeatureVossNarration(subclassId: string, featureName: string) {
+  const alias = featureNameAliases[`${subclassId}:${featureName}`] ?? featureName
+  return getGeminiDruidSubclassFeatureVossNarration(subclassId, alias)
+}
 
 export {
   druidClassVossComment,
