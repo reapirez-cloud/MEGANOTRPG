844bcaae1c3bad4125f02869469b03832dfc74b3
reapirez-cloud <reapirez@gmail.com>
2026-09-03 02:18:09 +0300

    feat(wizard): activate Enchantment Conjuration War Magic Voss stories

--- src/data/classes/wizardSubclassVossNarration.ts
+++ src/data/classes/wizardSubclassVossNarration.ts
@@ -9,20 +9,30 @@ import {
   getWizardGeminiPack1SubclassVossNarration,
   wizardGeminiPack1Coverage,
 } from "./wizardSubclassVossNarrationGeminiPack1.ts"
+import {
+  applyWizardGeminiPack3FeatureStories,
+  getWizardGeminiPack3SubclassVossComment,
+  getWizardGeminiPack3SubclassVossNarration,
+  wizardGeminiPack3Coverage,
+} from "./wizardSubclassVossNarrationGeminiPack3.ts"
 
 applyWizardGeminiPack1FeatureStories()
+applyWizardGeminiPack3FeatureStories()
 
 export function getWizardSubclassVossNarration(subclassId: string) {
-  return getWizardGeminiPack1SubclassVossNarration(subclassId)
+  return getWizardGeminiPack3SubclassVossNarration(subclassId)
+    || getWizardGeminiPack1SubclassVossNarration(subclassId)
     || getBaseWizardSubclassVossNarration(subclassId)
 }
 
 export function getWizardSubclassVossComment(subclassId: string) {
-  return getWizardGeminiPack1SubclassVossComment(subclassId)
+  return getWizardGeminiPack3SubclassVossComment(subclassId)
+    || getWizardGeminiPack1SubclassVossComment(subclassId)
     || getBaseWizardSubclassVossComment(subclassId)
 }
 
 export const wizardSubclassVossNarrationCoverage = Array.from(new Set([
+  ...wizardGeminiPack3Coverage,
   ...wizardGeminiPack1Coverage,
   ...baseWizardSubclassVossNarrationCoverage,
 ]))
